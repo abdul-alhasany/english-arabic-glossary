db2bd9800192d92ec8233863d0cfde1bfed4dfa0
Abdul Al-Hasany <kahhar87@gmail.com>
2025-05-27 08:42:40 +0300

    fix: update base

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools';
 
 // https://vite.dev/config/
 export default defineConfig({
-	base: 'english-arabic-glossary',
+	base: '/english-arabic-glossary',
 	plugins: [
 		vue(),
 		vueDevTools(),
